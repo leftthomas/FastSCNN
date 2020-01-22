1ec92d63fdd627f9f6abb41279b372511761e877
leftthomas <leftthomas@qq.com>
2020-01-22 21:46:39 +0800

    update dataset

--- cityscapes.py
+++ cityscapes.py
@@ -1,16 +1,17 @@
-"""Prepare Cityscapes dataset"""
+"""Cityscapes Dataloader"""
 import os
+import random
 
 import numpy as np
 import torch
-from PIL import Image
+import torch.utils.data as data
+from PIL import Image, ImageOps, ImageFilter
 
-from segbase import SegmentationDataset
+__all__ = ['CitySegmentation']
 
 
-class CitySegmentation(SegmentationDataset):
+class CitySegmentation(data.Dataset):
     """Cityscapes Semantic Segmentation Dataset.
-
     Parameters
     ----------
     root : string
@@ -38,14 +39,19 @@ class CitySegmentation(SegmentationDataset):
     BASE_DIR = 'cityscapes'
     NUM_CLASS = 19
 
-    def __init__(self, root='../datasets/citys', split='train', mode=None, transform=None, **kwargs):
-        super(CitySegmentation, self).__init__(root, split, mode, transform, **kwargs)
-        # self.root = os.path.join(root, self.BASE_DIR)
-        assert os.path.exists(self.root), "Please setup the dataset using ../datasets/cityscapes.py"
+    def __init__(self, root='./datasets/citys', split='train', mode=None, transform=None,
+                 base_size=520, crop_size=480, **kwargs):
+        super(CitySegmentation, self).__init__()
+        self.root = root
+        self.split = split
+        self.mode = mode if mode is not None else split
+        self.transform = transform
+        self.base_size = base_size
+        self.crop_size = crop_size
         self.images, self.mask_paths = _get_city_pairs(self.root, self.split)
         assert (len(self.images) == len(self.mask_paths))
         if len(self.images) == 0:
-            raise RuntimeError("Found 0 images in subfolders of:" + root + "\n")
+            raise RuntimeError("Found 0 images in subfolders of: " + self.root + "\n")
         self.valid_classes = [7, 8, 11, 12, 13, 17, 19, 20, 21, 22,
                               23, 24, 25, 26, 27, 28, 31, 32, 33]
         self._key = np.array([-1, -1, -1, -1, -1, -1,
@@ -57,7 +63,6 @@ class CitySegmentation(SegmentationDataset):
         self._mapping = np.array(range(-1, len(self._key) - 1)).astype('int32')
 
     def _class_to_index(self, mask):
-        # assert the value
         values = np.unique(mask)
         for value in values:
             assert (value in self._mapping)
@@ -82,7 +87,69 @@ class CitySegmentation(SegmentationDataset):
         # general resize, normalize and toTensor
         if self.transform is not None:
             img = self.transform(img)
-        return img, mask, os.path.basename(self.images[index])
+        return img, mask
+
+    def _val_sync_transform(self, img, mask):
+        outsize = self.crop_size
+        short_size = outsize
+        w, h = img.size
+        if w > h:
+            oh = short_size
+            ow = int(1.0 * w * oh / h)
+        else:
+            ow = short_size
+            oh = int(1.0 * h * ow / w)
+        img = img.resize((ow, oh), Image.BILINEAR)
+        mask = mask.resize((ow, oh), Image.NEAREST)
+        # center crop
+        w, h = img.size
+        x1 = int(round((w - outsize) / 2.))
+        y1 = int(round((h - outsize) / 2.))
+        img = img.crop((x1, y1, x1 + outsize, y1 + outsize))
+        mask = mask.crop((x1, y1, x1 + outsize, y1 + outsize))
+        # final transform
+        img, mask = self._img_transform(img), self._mask_transform(mask)
+        return img, mask
+
+    def _sync_transform(self, img, mask):
+        # random mirror
+        if random.random() < 0.5:
+            img = img.transpose(Image.FLIP_LEFT_RIGHT)
+            mask = mask.transpose(Image.FLIP_LEFT_RIGHT)
+        crop_size = self.crop_size
+        # random scale (short edge)
+        short_size = random.randint(int(self.base_size * 0.5), int(self.base_size * 2.0))
+        w, h = img.size
+        if h > w:
+            ow = short_size
+            oh = int(1.0 * h * ow / w)
+        else:
+            oh = short_size
+            ow = int(1.0 * w * oh / h)
+        img = img.resize((ow, oh), Image.BILINEAR)
+        mask = mask.resize((ow, oh), Image.NEAREST)
+        # pad crop
+        if short_size < crop_size:
+            padh = crop_size - oh if oh < crop_size else 0
+            padw = crop_size - ow if ow < crop_size else 0
+            img = ImageOps.expand(img, border=(0, 0, padw, padh), fill=0)
+            mask = ImageOps.expand(mask, border=(0, 0, padw, padh), fill=0)
+        # random crop crop_size
+        w, h = img.size
+        x1 = random.randint(0, w - crop_size)
+        y1 = random.randint(0, h - crop_size)
+        img = img.crop((x1, y1, x1 + crop_size, y1 + crop_size))
+        mask = mask.crop((x1, y1, x1 + crop_size, y1 + crop_size))
+        # gaussian blur as in PSP
+        if random.random() < 0.5:
+            img = img.filter(ImageFilter.GaussianBlur(
+                radius=random.random()))
+        # final transform
+        img, mask = self._img_transform(img), self._mask_transform(mask)
+        return img, mask
+
+    def _img_transform(self, img):
+        return np.array(img)
 
     def _mask_transform(self, mask):
         target = self._class_to_index(np.array(mask).astype('int32'))
@@ -90,6 +157,11 @@ class CitySegmentation(SegmentationDataset):
 
     def __len__(self):
         return len(self.images)
+
+    @property
+    def num_class(self):
+        """Number of categories."""
+        return self.NUM_CLASS
 
     @property
     def pred_offset(self):
@@ -102,7 +174,7 @@ def _get_city_pairs(folder, split='train'):
         mask_paths = []
         for root, _, files in os.walk(img_folder):
             for filename in files:
-                if filename.endswith('.png'):
+                if filename.endswith(".png"):
                     imgpath = os.path.join(root, filename)
                     foldername = os.path.basename(os.path.dirname(imgpath))
                     maskname = filename.replace('leftImg8bit', 'gtFine_labelIds')
@@ -133,3 +205,7 @@ def _get_city_pairs(folder, split='train'):
         mask_paths = train_mask_paths + val_mask_paths
     return img_paths, mask_paths
 
+
+if __name__ == '__main__':
+    dataset = CitySegmentation()
+    img, label = dataset[0]
