bce9610bd2d1d996b7c565687a6863d6f129282f
leftthomas <leftthomas@qq.com>
2020-02-01 21:58:28 +0800

    update model

--- model.py
+++ model.py
@@ -3,34 +3,36 @@ from torch import nn
 from torch.nn import functional as F
 
 
-class Fast_SCNN(torch.nn.Module):
-    def __init__(self, input_channel, num_classes):
+class FastSCNN(nn.Module):
+    def __init__(self, in_channels, num_classes):
         super().__init__()
 
-        self.learning_to_downsample = LearningToDownsample(input_channel)
+        self.learning_to_down_sample = LearningToDownSample(in_channels)
         self.global_feature_extractor = GlobalFeatureExtractor()
-        self.feature_fusion = FeatureFusionModule()
+        self.feature_fusion = FeatureFusion(scale_factor=4)
         self.classifier = Classifier(num_classes)
 
     def forward(self, x):
-        shared = self.learning_to_downsample(x)
+        shared = self.learning_to_down_sample(x)
         x = self.global_feature_extractor(shared)
         x = self.feature_fusion(shared, x)
         x = self.classifier(x)
         return x
 
 
-class LearningToDownsample(torch.nn.Module):
+class LearningToDownSample(nn.Module):
     def __init__(self, in_channels):
         super().__init__()
-        self.conv1 = ConvBlock(in_channels=in_channels, out_channels=32, stride=2)
-        self.sconv1 = nn.Sequential(
+        self.conv = ConvBlock(in_channels=in_channels, out_channels=32, stride=2)
+        self.dsconv1 = nn.Sequential(
+            # depthwise convolution
             nn.Conv2d(32, 32, kernel_size=3, stride=2, padding=1, dilation=1, groups=32, bias=False),
             nn.BatchNorm2d(32),
+            # pointwise convolution
             nn.Conv2d(32, 48, kernel_size=1, stride=1, padding=0, dilation=1, groups=1, bias=False),
             nn.BatchNorm2d(48),
             nn.ReLU(inplace=True))
-        self.sconv2 = nn.Sequential(
+        self.dsconv2 = nn.Sequential(
             nn.Conv2d(48, 48, kernel_size=3, stride=2, padding=1, dilation=1, groups=48, bias=False),
             nn.BatchNorm2d(48),
             nn.Conv2d(48, 64, kernel_size=1, stride=1, padding=0, dilation=1, groups=1, bias=False),
@@ -38,25 +40,25 @@ class LearningToDownsample(torch.nn.Module):
             nn.ReLU(inplace=True))
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.sconv1(x)
-        x = self.sconv2(x)
+        x = self.conv(x)
+        x = self.dsconv1(x)
+        x = self.dsconv2(x)
         return x
 
 
-class GlobalFeatureExtractor(torch.nn.Module):
+class GlobalFeatureExtractor(nn.Module):
     def __init__(self):
         super().__init__()
-        self.first_block = nn.Sequential(InvertedResidual(64, 64, 2, 6),
-                                         InvertedResidual(64, 64, 1, 6),
-                                         InvertedResidual(64, 64, 1, 6))
-        self.second_block = nn.Sequential(InvertedResidual(64, 96, 2, 6),
-                                          InvertedResidual(96, 96, 1, 6),
-                                          InvertedResidual(96, 96, 1, 6))
-        self.third_block = nn.Sequential(InvertedResidual(96, 128, 1, 6),
-                                         InvertedResidual(128, 128, 1, 6),
-                                         InvertedResidual(128, 128, 1, 6))
-        self.ppm = PSPModule(128, 128)
+        self.first_block = nn.Sequential(Bottleneck(64, 64, 2, 6),
+                                         Bottleneck(64, 64, 1, 6),
+                                         Bottleneck(64, 64, 1, 6))
+        self.second_block = nn.Sequential(Bottleneck(64, 96, 2, 6),
+                                          Bottleneck(96, 96, 1, 6),
+                                          Bottleneck(96, 96, 1, 6))
+        self.third_block = nn.Sequential(Bottleneck(96, 128, 1, 6),
+                                         Bottleneck(128, 128, 1, 6),
+                                         Bottleneck(128, 128, 1, 6))
+        self.ppm = PPMModule(128, 128)
 
     def forward(self, x):
         x = self.first_block(x)
@@ -66,40 +68,73 @@ class GlobalFeatureExtractor(torch.nn.Module):
         return x
 
 
-# Modified from https://github.com/tonylins/pytorch-mobilenet-v2
-class InvertedResidual(nn.Module):
-    def __init__(self, inp, oup, stride, expand_ratio):
-        super(InvertedResidual, self).__init__()
-        self.stride = stride
-        assert stride in [1, 2]
+class FeatureFusion(nn.Module):
+    def __init__(self, scale_factor):
+        super().__init__()
 
-        hidden_dim = round(inp * expand_ratio)
-        self.use_res_connect = self.stride == 1 and inp == oup
+        self.scale_factor = scale_factor
+        self.conv_high_res = nn.Conv2d(64, 128, kernel_size=1, stride=1, padding=0, bias=True)
 
-        if expand_ratio == 1:
-            self.conv = nn.Sequential(
-                # dw
-                nn.Conv2d(hidden_dim, hidden_dim, 3, stride, 1, groups=hidden_dim, bias=False),
-                nn.BatchNorm2d(hidden_dim),
-                nn.ReLU(inplace=True),
-                # pw-linear
-                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
-                nn.BatchNorm2d(oup),
-            )
-        else:
-            self.conv = nn.Sequential(
-                # pw
-                nn.Conv2d(inp, hidden_dim, 1, 1, 0, bias=False),
-                nn.BatchNorm2d(hidden_dim),
-                nn.ReLU(inplace=True),
-                # dw
-                nn.Conv2d(hidden_dim, hidden_dim, 3, stride, 1, groups=hidden_dim, bias=False),
-                nn.BatchNorm2d(hidden_dim),
-                nn.ReLU(inplace=True),
-                # pw-linear
-                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
-                nn.BatchNorm2d(oup),
-            )
+        self.dwconv = ConvBlock(in_channels=128, out_channels=128, stride=1, padding=scale_factor,
+                                dilation=scale_factor, groups=128)
+        self.conv_low_res = nn.Conv2d(128, 128, kernel_size=1, stride=1, padding=0, bias=True)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, high_res_input, low_res_input):
+        low_res_input = F.interpolate(input=low_res_input, scale_factor=self.scale_factor, mode='bilinear',
+                                      align_corners=True)
+        low_res_input = self.dwconv(low_res_input)
+        low_res_input = self.conv_low_res(low_res_input)
+
+        high_res_input = self.conv_high_res(high_res_input)
+        x = torch.add(high_res_input, low_res_input)
+        return self.relu(x)
+
+
+class Classifier(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.dsconv1 = ConvBlock(in_channels=128, out_channels=128, stride=1, dilation=1, groups=128)
+        self.dsconv2 = ConvBlock(in_channels=128, out_channels=128, stride=1, dilation=1, groups=128)
+        self.conv = nn.Conv2d(128, num_classes, kernel_size=1, stride=1, padding=0, bias=True)
+
+    def forward(self, x):
+        x = self.dsconv1(x)
+        x = self.dsconv2(x)
+        return self.conv(x)
+
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=2, padding=1, dilation=1, groups=1):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding,
+                              dilation=dilation, groups=groups, bias=False)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, input):
+        x = self.conv(input)
+        return self.relu(self.bn(x))
+
+
+class Bottleneck(nn.Module):
+    def __init__(self, in_channels, out_channels, stride, expand_ratio):
+        super().__init__()
+
+        hidden_dim = in_channels * expand_ratio
+        self.use_res_connect = stride == 1 and in_channels == out_channels
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, hidden_dim, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(hidden_dim),
+            nn.ReLU(inplace=True),
+            # depthwise convolution
+            nn.Conv2d(hidden_dim, hidden_dim, 3, stride, 1, groups=hidden_dim, bias=False),
+            nn.BatchNorm2d(hidden_dim),
+            nn.ReLU(inplace=True),
+            # pw-linear
+            nn.Conv2d(hidden_dim, out_channels, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(out_channels))
 
     def forward(self, x):
         if self.use_res_connect:
@@ -108,75 +143,24 @@ class InvertedResidual(nn.Module):
             return self.conv(x)
 
 
-# Modified from https://github.com/Lextal/pspnet-pytorch/blob/master/pspnet.py
-class PSPModule(nn.Module):
-    def __init__(self, features, out_features=1024, sizes=(1, 2, 3, 6)):
+class PPMModule(nn.Module):
+    def __init__(self, in_channels, out_channels, sizes=(1, 2, 3, 6)):
         super().__init__()
-        self.stages = []
-        self.stages = nn.ModuleList([self._make_stage(features, size) for size in sizes])
-        self.bottleneck = nn.Conv2d(features * (len(sizes) + 1), out_features, kernel_size=1)
-        self.relu = nn.ReLU()
 
-    def _make_stage(self, features, size):
+        inter_channels = in_channels // len(sizes)
+        assert in_channels % len(sizes) == 0
+
+        self.stages = nn.ModuleList([self._make_stage(in_channels, inter_channels, size) for size in sizes])
+        self.conv = ConvBlock(in_channels * 2, out_channels, kernel_size=1, stride=1, padding=0)
+
+    def _make_stage(self, in_channels, inter_channels, size):
         prior = nn.AdaptiveAvgPool2d(output_size=(size, size))
-        conv = nn.Conv2d(features, features, kernel_size=1, bias=False)
+        conv = ConvBlock(in_channels, inter_channels, kernel_size=1, stride=1, padding=0)
         return nn.Sequential(prior, conv)
 
     def forward(self, feats):
         h, w = feats.size(2), feats.size(3)
         priors = [F.interpolate(input=stage(feats), size=(h, w), mode='bilinear',
                                 align_corners=True) for stage in self.stages] + [feats]
-        # import pdb;pdb.set_trace()
-        bottle = self.bottleneck(torch.cat(priors, 1))
-        return self.relu(bottle)
-
-
-class FeatureFusionModule(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.sconv1 = ConvBlock(in_channels=128, out_channels=128, stride=1, dilation=1, groups=128)
-        self.conv_low_res = nn.Conv2d(128, 128, kernel_size=1, stride=1, padding=0, bias=True)
-
-        self.conv_high_res = nn.Conv2d(64, 128, kernel_size=1, stride=1, padding=0, bias=True)
-        self.relu = nn.ReLU()
-
-    def forward(self, high_res_input, low_res_input):
-        low_res_input = F.interpolate(input=low_res_input, scale_factor=4, mode='bilinear', align_corners=True)
-        low_res_input = self.sconv1(low_res_input)
-        low_res_input = self.conv_low_res(low_res_input)
-
-        high_res_input = self.conv_high_res(high_res_input)
-        x = torch.add(high_res_input, low_res_input)
-        return self.relu(x)
-
-
-class Classifier(torch.nn.Module):
-    def __init__(self, num_classes):
-        super().__init__()
-        self.sconv1 = ConvBlock(in_channels=128, out_channels=128, stride=1, dilation=1, groups=128)
-        self.sconv2 = ConvBlock(in_channels=128, out_channels=128, stride=1, dilation=1, groups=128)
-        self.conv = nn.Conv2d(128, num_classes, kernel_size=1, stride=1, padding=0, bias=True)
-
-    def forward(self, x):
-        x = self.sconv1(x)
-        x = self.sconv1(x)
-        return self.conv(x)
-
-
-class ConvBlock(torch.nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, stride=2, padding=1, dilation=1, groups=1):
-        super().__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding,
-                               dilation=dilation, groups=groups, bias=False)
-        self.bn = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU()
-
-    def forward(self, input):
-        x = self.conv1(input)
-        return self.relu(self.bn(x))
-
-
-if __name__ == '__main__':
-    model = Fast_SCNN(input_channel=3, num_classes=10)
-    x = torch.rand(2, 3, 256, 256)
-    y = model(x)
+        bottle = self.conv(torch.cat(priors, dim=1))
+        return bottle
